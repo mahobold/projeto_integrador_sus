ea9051534a52aa2f1ac5b1dbf9c2ca9f583f3ff0
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-03-30 11:53:12 -0300

    menu maria

--- menu.php
+++ menu.php
@@ -1,9 +1,9 @@
+<div class="logo">
+  <img src="logot.png">
+</div>
 <nav class="navbar navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
   <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="logo.png" width="30" height="24">
-    </a>
   </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -12,7 +12,7 @@
       <div class="offcanvas-header">
          <div class="container">
     <a class="navbar-brand" href="#">
-      <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" width="30" height="24">
+      <img src="logot.png" width="250">
     </a>
   </div>
         <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
@@ -23,20 +23,16 @@
             <a class="nav-link active" aria-current="page" href="#">Página Principal</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link active" aria-current="page" href="#">Unidades próximas</a>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu dropdown-menu-dark">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li>
-                <hr class="dropdown-divider">
-              </li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Médicos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Farmácia</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Info doenças</a>
           </li>
         </ul>
       </div>
