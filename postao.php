a501254921cce1e1b88483937e8f2686df6d22d8
LUIZ EDUARDO ROSAESKI BONFIM <bonfim.2979@aluno.pr.senac.br>
2023-03-30 11:35:27 -0300

    Branch luiz

--- postao.php
+++ postao.php
@@ -68,9 +68,65 @@
       <td>07:30 às 11:30</td>
       <td>13:00 às 17:00</td>
     </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>quinta-feira</td>
+      <td>07:30 às 11:30</td>
+      <td>13:00 às 17:00</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Sexta-feira</td>
+      <td>07:30 às 11:30</td>
+      <td>13:00 às 17:00</td>
+    </tr>
   </tbody>
 </table>
 
+<h2>POSTO DE SAUDE DE JACUTINGA</h2>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Dias</th>
+      <th scope="col">Horário/Manhã</th>
+      <th scope="col">Horário/Tarde</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Segunda-feira</td>    <!--colocar a tabela menor-->
+      <td>07:30 às 11:30</td>
+      <td>13:00 às 17:00</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>terça-feira</td>
+      <td>07:30 às 11:30</td>
+      <td>13:00 às 17:00</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>quarta-feira</td>
+      <td>07:30 às 11:30</td>
+      <td>13:00 às 17:00</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>quinta-feira</td>
+      <td>07:30 às 11:30</td>
+      <td>13:00 às 17:00</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Sexta-feira</td>
+      <td>07:30 às 11:30</td>
+      <td>13:00 às 17:00</td>
+    </tr>
+  </tbody>
+</table>
 
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
