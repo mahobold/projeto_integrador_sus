0ce2c357763bda59fb79f732b15850cdc7e3c0e3
ANA BEATRIZ FERREIRA PASSOS <passos.2967@aluno.pr.senac.br>
2023-03-31 08:40:56 -0300

    css menu

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+        <link rel="stylesheet" href="projeto.css">
         <title>Página - Principal</title>
     </head>
     <body>
--- menu.php
+++ menu.php
@@ -1,14 +1,14 @@
 <div class="logo">
-  <img src="logot.png">
+  <img class="logo"src="logot.png" width="300px">
 </div>
-<nav class="navbar navbar-dark bg-dark fixed-top">
+<nav class="navbar navbar-dark ">
   <div class="container-fluid">
   <div class="container">
   </div>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+    <div class="offcanvas offcanvas-end " tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
       <div class="offcanvas-header">
          <div class="container">
     <a class="navbar-brand" href="#">
