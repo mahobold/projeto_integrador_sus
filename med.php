c24c51de756dad61e1e4935646d995e5224d3107
LETICIA ARAUJO PEREIRA <leticia.1970@aluno.pr.senac.br>
2023-04-28 08:47:07 -0300

    salvar

---
+++ med.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página profissões</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+    include ("menu.php")
+    ?>
+      <br>
+      <br>
+      <br>
+      <div id="carouselExampleFade" class="carousel slide carousel-fade">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="imagens\logo_sus.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="imagens\sistema.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="imagens\SUS.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+     <br>
+     <br>
+     <br>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.css" integrity="sha512-6lLUdeQ5uheMFbWm3CP271l14RsX1xtx+J5x2yeIDkkiBpeVTNhTqijME7GgRKKi6hCqovwCoBTlRBEC20M8Mg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+
+      <div class="container">
+        <h1>Plantão do dia</h1>
+        <div class="row slider">
+        <div class="col-md-12">
+            <div class="details">
+            <div class="card" style="width: 18rem;">
+                <img src="imagens\medico1.jpg" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Thayane Jukosky</h5>
+                </div>
+              </div>
+            <a href="imagens\Currículo de administração azul masculino.pdf">Ver formação</a>
+        </div>
+    </div>
+        <div class="col-md-12">
+            <div class="details">
+                <div class="card" style="width: 18rem;">
+                    <img src="imagens\medico2.jpg" class="card-img-top">
+                    <div class="card-body">
+                      <h5 class="card-title">Victor Araújo</h5>
+                    </div>
+                  </div>
+                <a href="imagens\curriculo2.pdf">Ver formação</a>
+            </div>
+      </div>
+      <div class="col-md-12">
+        <div class="details">
+            <div class="card" style="width: 18rem;">
+                <img src="imagens\medico3.jpg" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Isadora Bertotti</h5>
+                </div>
+              </div>
+            <a href="imagens\curriculo3.pdf">Ver formação</a>
+        </div>
+    </div>
+    <div class="col-md-12">
+        <div class="details">
+            <div class="card" style="width: 18rem;">
+                <img src="imagens\medico4.jpg" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Esveraldo Camargo</h5>
+                </div>
+              </div>
+            <a href="imagens\curriculo4.pdf">Ver formação</a>
+    </div>
+    </div>
+    <div class="col-md-12">
+        <div class="details">
+            <div class="card" style="width: 18rem;">
+                <img src="imagens\medico5.jpg" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Agusti Zanoni</h5>
+                </div>
+              </div>
+            <a href="https://a.co/d/2sQhPbc">Ver formação</a>
+    </div>
+    </div>
+    <div class="col-md-12">
+        <div class="details">
+            <div class="card" style="width: 18rem;">
+                <img src="imagens\medico6.jpg" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Joaquim Sppadrizani</h5>
+                </div>
+              </div>
+            <a href="imagens\curriculo6.pdf">Ver formação</a>
+    </div>
+    </div>
+     </div>
+    </div>
+    <div class="container">
+      <h1>Plantão da tarde</h1>
+      <div class="row slider">
+      <div class="col-md-12">
+          <div class="details">
+          <div class="card" style="width: 18rem;">
+              <img src="imagens\medico7.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Carlos Vicente</h5>
+              </div>
+            </div>
+          <a href="imagens\curriculo7.pdf">Ver formação</a>
+      </div>
+  </div>
+      <div class="col-md-12">
+          <div class="details">
+              <div class="card" style="width: 18rem;">
+                  <img src="imagens\medico8.jpg" class="card-img-top">
+                  <div class="card-body">
+                    <h5 class="card-title">Alex Gourmet</h5>
+                  </div>
+                </div>
+              <a href="https://a.co/d/clXaASM">Ver formação</a>
+          </div>
+    </div>
+    <div class="col-md-12">
+      <div class="details">
+          <div class="card" style="width: 18rem;">
+              <img src="imagens\medico9.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Erick Jaquicn</h5>
+              </div>
+            </div>
+          <a href="https://a.co/d/47Rgrl2">Ver formação</a>
+      </div>
+  </div>
+  <div class="col-md-12">
+      <div class="details">
+          <div class="card" style="width: 18rem;">
+              <img src="imagens\medico10.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Elisandra Bertotti</h5>
+              </div>
+            </div>
+          <a href="https://a.co/d/hJoQ23L">Ver formação</a>
+  </div>
+  </div>
+  <div class="col-md-12">
+      <div class="details">
+          <div class="card" style="width: 18rem;">
+              <img src="imagens\medico11.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Sabrina Ferreira</h5>
+              </div>
+            </div>
+          <a href="https://a.co/d/2sQhPbc">Ver formação</a>
+  </div>
+  </div>
+  <div class="col-md-12">
+      <div class="details">
+          <div class="card" style="width: 18rem;">
+              <img src="imagens\medico12.jpg" class="card-img-top">
+              <div class="card-body">
+                <h5 class="card-title">Angel Muchiuti</h5>
+              </div>
+            </div>
+          <a href="https://a.co/d/d41MlUx">Ver formação</a>
+  </div>
+  </div>
+   </div>
+  </div>
+  <div class="container">
+    <h1>Plantão da noite</h1>
+    <div class="row slider">
+    <div class="col-md-12">
+        <div class="details">
+        <div class="card" style="width: 18rem;">
+            <img src="imagens\medico13.jpg" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">Carla Vicente</h5>
+            </div>
+          </div>
+        <a href="https://a.co/d/ektPUiK">Ver formação</a>
+    </div>
+</div>
+    <div class="col-md-12">
+        <div class="details">
+            <div class="card" style="width: 18rem;">
+                <img src="imagens\medico14.jpg" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Alessandra Souza</h5>
+                </div>
+              </div>
+            <a href="https://a.co/d/clXaASM">Ver formação</a>
+        </div>
+  </div>
+  <div class="col-md-12">
+    <div class="details">
+        <div class="card" style="width: 18rem;">
+            <img src="imagens\medico15.jpg" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">Isadora agatsuma</h5>
+            </div>
+          </div>
+        <a href="https://a.co/d/47Rgrl2">Ver formação</a>
+    </div>
+</div>
+<div class="col-md-12">
+    <div class="details">
+        <div class="card" style="width: 18rem;">
+            <img src="imagens\medico16.jpg" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">Elisandra Bertotti</h5>
+            </div>
+          </div>
+        <a href="https://a.co/d/hJoQ23L">Ver formação</a>
+</div>
+</div>
+<div class="col-md-12">
+    <div class="details">
+        <div class="card" style="width: 18rem;">
+            <img src="imagens\medico17.jpg" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">Angela Ferreira</h5>
+            </div>
+          </div>
+        <a href="https://a.co/d/2sQhPbc">Ver formação</a>
+</div>
+</div>
+<div class="col-md-12">
+    <div class="details">
+        <div class="card" style="width: 18rem;">
+            <img src="imagens\medico18.jpg" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">Angel Muchiuti</h5>
+            </div>
+          </div>
+        <a href="https://a.co/d/d41MlUx">Ver formação</a>
+</div>
+</div>
+ </div>
+</div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+        <script type="text/javascript">
+            $('.slider').slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+        </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
