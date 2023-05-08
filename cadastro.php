d4bf402a87a4163ac1082c5bd108efbc5b8a4097
MARIA VITORIA MAGALHAES HOBOLD <hobold.7994@aluno.pr.senac.br>
2023-05-08 08:57:22 -0300

    teste main

--- cadastro.php
+++ cadastro.php
@@ -13,7 +13,7 @@
     </head>
     <body>
         <?php
-           // include("menu.php");
+           // include("menu.php") mains;
         ?>
         <div class="container">
             <div class="azulmarinho"> 
